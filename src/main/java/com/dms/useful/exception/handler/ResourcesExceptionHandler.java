f70a41407e73ef5d1c69bdde99cd1165bee0fee8
Diorgenes Morais <diorgenesmorais@gmail.com>
2019-01-01 19:23:35 -0300

    passará a capturar HttpMessageNotReadableException

--- src/main/java/com/dms/useful/exception/handler/ResourcesExceptionHandler.java
+++ src/main/java/com/dms/useful/exception/handler/ResourcesExceptionHandler.java
@@ -72,6 +72,7 @@ public abstract class ResourcesExceptionHandler extends ResponseEntityExceptionH
 		return erros;
 	}
 
+	@ExceptionHandler({ HttpMessageNotReadableException.class })
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
